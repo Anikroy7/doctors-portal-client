eebbaa83493d0e068dc1d547dbdf0df40d05af35
Anikroy7 <roy645800@gmail.com>
2022-05-14 18:14:15 +0600

    login form added by using react firebase form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -9,13 +9,70 @@ const Login = () => {
     if (user) {
         console.log(user);
     }
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <div className='flex justify-center items-center h-screen'>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-100 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="text-center font-bold text-xl text-accent">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Your Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Type your email"
+                                class="input input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is required'
+                                    },
+                                    pattern: {
+                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                        message: 'error message'
+                                    }
+                                })}
+                            />
+                            <label class="label">
+                                {errors.email?.type === 'required' && <span class="label-text-alt text-red-700">{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span class="label-text-alt text-red-400">{errors.email.message}</span>}
+
+                            </label>
+                        </div>
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Your Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Type your Password"
+                                class="input input-bordered w-full max-w-xs"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'password is required'
+                                    },
+                                    minLength: {
+                                        value: 6,
+                                        message: 'Must be six characters or long'
+                                    }
+                                })}
+                            />
+                            <label class="label">
+                                {errors.password?.type === 'required' && <span class="label-text-alt text-red-700">{errors.password?.message}</span>}
+                                {errors.password?.type === 'minLength' && <span class="label-text-alt text-red-400">{errors.password?.message}</span>}
+
+                            </label>
+                        </div>
+                        <input />
+
+                        <input className='btn text-white w-full max-w-xs' type="submit" value='Log in' />
+                    </form>
                     <div className="divider">OR</div>
-                    <button onClick={() => signInWithGoogle()} className="btn btn-outline">Sing with Gmail</button>
+                    <button onClick={() => signInWithGoogle()} className="btn btn-outline">Continue With Gmail</button>
                 </div>
             </div>
         </div>
